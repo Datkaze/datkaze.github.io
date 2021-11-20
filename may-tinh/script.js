28de172e8cecef5db8432ec8b67430f902c176cc
Datkaze <datkakaze2@gmail.com>
2021-11-20 19:13:39 +0700

    fix

--- may-tinh/script.js
+++ may-tinh/script.js
@@ -3,59 +3,11 @@ let calculation = document.getElementById("show");
 function ac(){
   calculation.innerText = "";
   }
-  function number0() {
-    calculation.innerText += "0";
-  }
-  function number1() {
-    calculation.innerText += "1";
-  }
-  function number2() {
-    calculation.innerText += "2";
-  }
-  function number3() {
-    calculation.innerText += "3";
-  }
-  function number4() {
-    calculation.innerText += "4";
-  }
-  function number5() {
-    calculation.innerText += "5";
-  }
-  function number6() {
-    calculation.innerText += "6";
-  }
-  function number7() {
-    calculation.innerText += "7";
-  }
-  function number8() {
-    calculation.innerText += "8";
-  }
-  function number9() {
-    calculation.innerText += "9";
-  }
-  function dot() {
-    calculation.innerText += ".";
-  }
-  function percent() {
-    calculation.innerText += "%";
-  }
-  function square() {
-   x = calculation.innerText += "^"
-  }
-  function especially1() {
-    calculation.innerText += "+";
-  }
-  function especially2(){
-    calculation.innerText += "-";
-  }
-  function especially3(){
-    calculation.innerText += "×";
-  }
-  function especially4(){
-    calculation.innerText += "÷";
+  function math(number){
+    calculation.innerText += number;
   }
   function equal(){
-    let result = calculation.innerHTML.replaceAll("×", "*").replaceAll("÷", "/").replaceAll("^", "**2");
+    let result = calculation.innerHTML.replaceAll("×", "*").replaceAll("÷", "/").replaceAll("^", "**2").replaceAll("%", "/100");
    calculation.innerHTML = eval(result).toFixed(2).replace(/\.?0*$/g, '');
   }
 
