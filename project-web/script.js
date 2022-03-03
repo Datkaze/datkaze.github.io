538dda14bb859252765f341fb48aec48f87f1de5
Datkaze <datkakaze2@gmail.com>
2022-03-03 22:49:05 +0700

    push

--- project-web/script.js
+++ project-web/script.js
@@ -97,7 +97,6 @@ $('.item-slider-tc').slick({
   });
 
 
-
   $(() => {
     $(".slice-slider-ctsp2 img").click(function(){
       let imgPath = $(this).attr('src');
@@ -109,31 +108,31 @@ $('.item-slider-tc').slick({
 
 
 
-let cartForm = document.querySelector('.shopping-cart');
-let cartBtn = document.querySelector('#cart-btn');
-let menuBtn = document.querySelector('#menu-btn');
+// let cartForm = document.querySelector('.shopping-cart');
+// let cartBtn = document.querySelector('#cart-btn');
+// let menuBtn = document.querySelector('#menu-btn');
 
-document.querySelector('#search-btn').onclick = () =>{
-    searchForm.classList.toggle('active');
-}
-
-document.querySelector('#cart-btn').onclick = () =>{
-    cartForm.classList.toggle('active');
-}
-
-
-// document.querySelector('#menu-btn').onclick = () =>{
-//     navbar.classList.toggle('active');
+// document.querySelector('#search-btn').onclick = () =>{
+//     searchForm.classList.toggle('active');
 // }
 
-menuBtn.addEventListener('click', ()=>{
-    menuBtn.classList.toggle('fa-times');
-    navbar.classList.toggle('active');
-})
+// document.querySelector('#cart-btn').onclick = () =>{
+//     cartForm.classList.toggle('active');
+// }
 
 
-window.onscroll = () =>{
-    searchForm.classList.remove('active');
-    cartForm.classList.remove('active');
+// // document.querySelector('#menu-btn').onclick = () =>{
+// //     navbar.classList.toggle('active');
+// // }
+
+// menuBtn.addEventListener('click', ()=>{
+//     menuBtn.classList.toggle('fa-times');
+//     navbar.classList.toggle('active');
+// })
+
+
+// window.onscroll = () =>{
+//     searchForm.classList.remove('active');
+//     cartForm.classList.remove('active');
     
-}
+// }
