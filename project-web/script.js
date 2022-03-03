ae02be36bd024906f6377b7cfa45e8f000fd35b2
Datkaze <datkakaze2@gmail.com>
2022-03-03 22:48:13 +0700

    push

--- project-web/script.js
+++ project-web/script.js
@@ -104,3 +104,36 @@ $('.item-slider-tc').slick({
       $("#main-img").attr('src', imgPath);
     })
   })
+
+
+
+
+
+let cartForm = document.querySelector('.shopping-cart');
+let cartBtn = document.querySelector('#cart-btn');
+let menuBtn = document.querySelector('#menu-btn');
+
+document.querySelector('#search-btn').onclick = () =>{
+    searchForm.classList.toggle('active');
+}
+
+document.querySelector('#cart-btn').onclick = () =>{
+    cartForm.classList.toggle('active');
+}
+
+
+// document.querySelector('#menu-btn').onclick = () =>{
+//     navbar.classList.toggle('active');
+// }
+
+menuBtn.addEventListener('click', ()=>{
+    menuBtn.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+})
+
+
+window.onscroll = () =>{
+    searchForm.classList.remove('active');
+    cartForm.classList.remove('active');
+    
+}
